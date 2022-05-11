8f1d87440d10fae0fc5505d84e1737fc7c307d5b
Dana Huffman <dana.huffman@yahoo.com>
2022-05-11 13:12:50 -0700

    Completed Challenge 5

--- javascript/code-challenges/challenges-03.test.js
+++ javascript/code-challenges/challenges-03.test.js
@@ -64,6 +64,7 @@ For example, notInFirstArray([1,2,3], [1,2,3,4]) returns [4].
 
 const notInFirstArray = (forbiddenValues, arr) => {
   // Solution code here...
+  return arr.filter(element => forbiddenValues.indexOf(element) === -1);
 };
 
 /* ------------------------------------------------------------------------------------------------
