46e5fe281f5b171b4c5e6c1c8fc852e595d3f9c8
Dana Huffman <dana.huffman@yahoo.com>
2022-05-11 13:01:28 -0700

    Completed Challenge 3

--- javascript/code-challenges/challenges-03.test.js
+++ javascript/code-challenges/challenges-03.test.js
@@ -38,6 +38,7 @@ For example, containsAnd(['panda', 'ran', 'and']) returns ['panda', 'and'].
 
 const containsAnd = (arr) => {
   // Solution code here...
+  return arr.filter(element => element.includes('and'));
 };
 
 /* ------------------------------------------------------------------------------------------------
