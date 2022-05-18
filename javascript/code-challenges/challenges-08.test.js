cffa010291b114d8cc13ce87edcf89dbc190af49
Dana Huffman <dana.huffman@yahoo.com>
2022-05-18 14:49:03 -0700

    Completed Challenge 4

--- javascript/code-challenges/challenges-08.test.js
+++ javascript/code-challenges/challenges-08.test.js
@@ -107,6 +107,8 @@ Write a function named containsWorld that takes in a string or number of any len
 
 const containsWorld = (input) => {
   // Solution code here...
+  let regex = /(world)/;
+  return regex.test(input);
 };
 
 /* ------------------------------------------------------------------------------------------------
