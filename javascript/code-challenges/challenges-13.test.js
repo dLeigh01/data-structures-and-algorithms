5fc038b1ce558817324da26ba7576d9e7a2f1e1b
Dana Huffman <dana.huffman@yahoo.com>
2022-05-25 16:36:47 -0700

    Completed Challenge 3

--- javascript/code-challenges/challenges-13.test.js
+++ javascript/code-challenges/challenges-13.test.js
@@ -42,6 +42,7 @@ For example, ['this is great :)', 'wow', 'whyyyyyy :(', ':)))))'] returns ['this
 
 const findHappiness = (arr) => {
   // Solution code here...
+  return arr.filter(str => str.includes(':)'));
 };
 
 /* ------------------------------------------------------------------------------------------------
