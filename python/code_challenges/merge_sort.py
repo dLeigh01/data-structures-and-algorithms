79e587c0821ddf5cc6b1dc7503effe75bbe64227
Dana Huffman <dana.huffman@yahoo.com>
2022-08-23 14:08:19 -0700

    Completed code

---
+++ python/code_challenges/merge_sort.py
@@ -0,0 +1,34 @@
+def merge_sort(nums):
+  """Sorts a list of numbers recursively from lowest to highest
+  """
+  length = len(nums)
+
+  if length > 1:
+    mid = int(length / 2)
+    left = nums[:mid]
+    right = nums[mid:]
+
+    merge_sort(left)
+    merge_sort(right)
+    return merge(left, right, nums)
+
+def merge(left, right, nums):
+  left_index = 0
+  right_index = 0
+  nums_index = 0
+
+  while left_index < len(left) and right_index < len(right):
+    if left[left_index] <= right[right_index]:
+      nums[nums_index] = left[left_index]
+      left_index += 1
+    else:
+      nums[nums_index] = right[right_index]
+      right_index += 1
+    nums_index += 1
+
+  if left_index == len(left):
+    nums[nums_index:] = right[right_index:]
+  else:
+    nums[nums_index:] = left[left_index:]
+
+  return nums
