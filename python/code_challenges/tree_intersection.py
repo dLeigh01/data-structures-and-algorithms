75b934d96659d922c5f489fa0875373bbfc6ec63
Dana Huffman <dana.huffman@yahoo.com>
2022-08-30 14:58:22 -0700

    all tests passing

--- python/code_challenges/tree_intersection.py
+++ python/code_challenges/tree_intersection.py
@@ -1,5 +1,43 @@
 from data_structures.binary_tree import BinaryTree
+from data_structures.hashtable import Hashtable
+from data_structures.queue import Queue
 
 
-def tree_intersection():
-    pass
+def tree_intersection(tree1, tree2):
+    checker = Hashtable()
+    values = []
+
+    breadth_hash_search(tree1, checker)
+    check_hash_contains(tree2, checker, values)
+
+    return set(values)
+
+
+def check_hash_contains(tree, checker, values):
+    queue = Queue()
+    if tree.root is not None:
+        queue.enqueue(tree.root)
+
+    while queue.is_empty() is False:
+        node = queue.dequeue()
+        if checker.contains(str(node.value)):
+            values.append(node.value)
+        if node.right:
+            queue.enqueue(node.left)
+        if node.right:
+            queue.enqueue(node.right)
+
+
+def breadth_hash_search(tree, checker):
+    queue = Queue()
+    if tree.root is not None:
+        queue.enqueue(tree.root)
+
+    while queue.is_empty() is False:
+        node = queue.dequeue()
+        checker.set(str(node.value), 1)
+        if node.left:
+            queue.enqueue(node.left)
+        if node.right:
+            queue.enqueue(node.right)
+
---
+++ python/docs/tree_intersection/README.md
@@ -0,0 +1,14 @@
+# Challenge Summary
+
+Create a function that takes in two binary trees and returns a set of all common values between them.
+
+## Whiteboard Process
+
+![whiteboard](./tree-intersection.jpg)
+
+## Approach & Efficiency
+
+## Solution
+
+[code](../../code_challenges/tree_intersection.py) |
+[tests](../../tests/code_challenges/test_tree_intersection.py)
